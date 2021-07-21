a923017a600c9754f962b73c77f0917ca0d54eb2
Bill <billfeng2002@gmail.com>
2021-07-20 22:02:29 -0700

    commit tree 8423227fdcc8ee28502fa883df58742a5904e9ef

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+let main=document.getElementById('main')
+main.remove()
+
+var newHeader =document.createElement("h1")
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Bill is the champion" 
+
+document.body.appendChild(newHeader)
